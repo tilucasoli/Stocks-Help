398612816cdb96734811ccce36e0b5e911b7e78c
Lucas Oliveira <tilucasoli@gmail.com>
2020-08-13 09:31:39 -0300

    MVC Pattern and the paste Utils

---
+++ Stocks Help/Utils/ProjectColors.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectColors.swift
+//  Stocks Help
+//
+//  Created by Lucas Oliveira on 13/08/20.
+//  Copyright © 2020 Lucas Oliveira. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let purpleSH = UIColor(red: 140, green: 80, blue: 216, alpha: 1)
+    static let redSH = UIColor(red: 255, green: 92, blue: 105, alpha: 1)
+    static let backgroundSH = UIColor(red: 249, green: 247, blue: 244, alpha: 1)
+    static let textColorSH = UIColor(red: 29, green: 35, blue: 67, alpha: 1)
+}
